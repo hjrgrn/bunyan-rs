614cc254ab94ff6a39b9fc3d866d3005d44be28a
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2025-04-03 15:41:04 +0200

    benchmark: include different formats

--- benchmark_js.sh
+++ benchmark_js.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 cat $1 | npx bunyan
+cat $1 | npx bunyan -o short
+cat $1 | npx bunyan -o json-10
+cat $1 | npx bunyan -o bunyan
--- benchmark_rs.sh
+++ benchmark_rs.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
 cat ${1} | ./target/release/bunyan
+cat ${1} | ./target/release/bunyan -o short
+cat ${1} | ./target/release/bunyan -o json-10
+cat ${1} | ./target/release/bunyan -o bunyan
